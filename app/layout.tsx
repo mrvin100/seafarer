2cad599823a230bf62f2542209d69064c2dfe2f8
mrvin100 <vincentyoumssi@example.com>
2025-01-17 09:44:41 +0100

    add: fonts variables to global css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,8 @@
 import type { Metadata } from "next";
-import { Cormorant_Garamond, Cormorant_Upright, Inconsolata, Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Footer, Header } from "@/components/global";
 import { Toaster } from "@/components/ui/sonner";
-
-const cormorant_garamond = Cormorant_Garamond({
-  variable: "--font-cormorant-garamont",
-  subsets: ["latin"],
-  weight: ['300', '400', '500', '600', '700']
-});
-const cormorant_upright = Cormorant_Upright({
-  variable: "--font-cormorant-upright",
-  subsets: ["latin"],
-  weight: ['300', '400', '500', '600', '600', '700']
-});
-
-const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "500", "700"],
-});
-
-const inconsolata = Inconsolata({
-  variable: "--font-inconsolata",
-  subsets: ["latin"],
-  weight: [ '200', "300", "400", "500", '600', "700"],
-});
 
 export const metadata: Metadata = {
   title: "Dark Home - Seafarer",
@@ -41,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressContentEditableWarning={true}>
       <body
-        className={`${roboto.variable} font-extralight ${cormorant_garamond.variable}  ${cormorant_upright.variable}   ${inconsolata.className} antialiased tracking-wider`}
+        className={`font-extralight antialiased tracking-wider`}
       >
         <ThemeProvider
           attribute="class"
